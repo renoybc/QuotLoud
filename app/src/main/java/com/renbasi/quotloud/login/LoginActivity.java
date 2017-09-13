2afebd327948024c273d69a2e9d7f6fadc76a449
renoybc <renoybc@yahoo.com>
2017-09-13 19:56:35 +0530

    commit2

---
+++ app/src/main/java/com/renbasi/quotloud/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.renbasi.quotloud.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.renbasi.quotloud.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
