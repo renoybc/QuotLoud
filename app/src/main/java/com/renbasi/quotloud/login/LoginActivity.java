b96bd90661edf7d2fcb176da40b9a89fea4ca3f8
renoybc <renoybc@yahoo.com>
2017-09-18 19:53:46 +0530

    login and reg dne

--- app/src/main/java/com/renbasi/quotloud/login/LoginActivity.java
+++ app/src/main/java/com/renbasi/quotloud/login/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.renbasi.quotloud.login;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,106 +12,150 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.renbasi.quotloud.Main.Homescreen;
+import com.renbasi.quotloud.Main.ResetPasswordActivity;
 import com.renbasi.quotloud.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
-    private Button btnSignIn, btnSignUp, btnResetPassword;
-    private ProgressBar progressBar;
-    private FirebaseAuth auth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText inputEmail,inputPassword;
+    private Button loginButton,registerButton,resetpassswordButton;
+    private ProgressDialog loginprogress;
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
-        //Get Firebase auth instance
-        auth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        usersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
+        loginprogress = new ProgressDialog(this);
 
-        btnSignIn = (Button) findViewById(R.id.sign_in_button);
-        btnSignUp = (Button) findViewById(R.id.sign_up_button);
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
+        loginButton = (Button) findViewById(R.id.login);
+        registerButton = (Button) findViewById(R.id.register);
+        resetpassswordButton = (Button) findViewById(R.id.resetpassword);
 
-        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                checklogin();
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                //startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                registerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(registerIntent);
+
+            }
+        });
+        resetpassswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent registerIntent = new Intent(LoginActivity.this, ResetPasswordActivity.class);
+                registerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(registerIntent);
 
             }
         });
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                finish();
-
-            }
-        });
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (password.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-                //create user
-                auth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                Toast.makeText(LoginActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
-                                progressBar.setVisibility(View.GONE);
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    Toast.makeText(LoginActivity.this, "Authentication failed." + task.getException(),
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    //startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                    finish();
-                                }
-
-                            }
-                        });
-
-            }
-        });
+    }
+    public final static boolean isValidEmail(CharSequence target)
+    {
+        if (TextUtils.isEmpty(target))
+        {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        progressBar.setVisibility(View.GONE);
+    private void checklogin() {
+
+        String Email_id = inputEmail.getText().toString().trim();
+        String Password = inputPassword.getText().toString().trim();
+
+        CharSequence temp_emilID=Email_id;//here username is the your edittext object...
+        if(!isValidEmail(temp_emilID))
+        {
+            inputEmail.requestFocus();
+            inputEmail.setError("Enter a valid emailid");
+
+        }
+        else
+        {
+            if(!TextUtils.isEmpty(Email_id) && !TextUtils.isEmpty(Password)){
+
+                loginprogress.setMessage("Checking Login....");
+                loginprogress.show();
+                mAuth.signInWithEmailAndPassword(Email_id,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            loginprogress.dismiss();
+                            checkUserExist();
+
+                        }
+                        else {
+                            loginprogress.dismiss();
+                            Toast.makeText(LoginActivity.this,"Error in login",Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+
+
+        }
+
+    }
+
+    private void checkUserExist() {
+
+        final String User_id = mAuth.getCurrentUser().getUid();
+        usersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(User_id)){
+
+                    Intent mainIntent = new Intent(LoginActivity.this, Homescreen.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mainIntent);
+
+                }else
+                {
+                    Toast.makeText(LoginActivity.this,"You need to setup your account",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
